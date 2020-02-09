8585f997359db8e992f30b3ccc7e46a2a1102871
MarkPaulin <paulin.mark@googlemail.com>
2020-02-09 22:05:06 +0000

    first commit

---
+++ server.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(tidyverse)
+
+df <- read_csv("Qualification landscape all levels all sector subject areas all qualification types.csv")
+cols <- ncol(df)
+
+
+
+server <- function(input, output, session) {
+  filtered_df <- reactive({
+    df2 <- df
+    if (input$search != "") {
+      df2 <- df2 %>% 
+        filter(str_detect(Title, regex(input$search, ignore_case = TRUE)))
+    }
+    df2 %>% 
+      filter(Level == input$level | input$level == "All levels") %>% 
+      filter(`Sector Subject Area` == input$ssa | input$ssa == "All sector subject areas") %>% 
+      filter(`Included in performance tables` == input$pt | input$pt == "All qualifications") %>% 
+      filter(input$type == "all" | (Type != 'GCSE' & Type != 'AS/A level')) %>% 
+      arrange(desc(Certificates))
+  })
+  
+  plot_df <- reactive({
+     filtered_df() %>% 
+      group_by(Type) %>% 
+      summarise(Certificates = sum(Certificates),
+                `Available qualifications` = n()) %>% 
+      mutate(Type = fct_reorder(Type, !!sym(input$order))) %>% 
+      pivot_longer(cols = -Type) %>% 
+      group_by(name) %>% 
+      mutate(perc = value / sum(value)) %>% 
+      ungroup() %>% 
+      mutate(label = paste0(scales::comma(value), " (", 100 * perc, "%)")) %>% 
+      mutate(perc = if_else(name == "Certificates", perc, -perc))
+  })
+  
+  
+  output$plot <- renderPlot({
+    plot_df() %>% 
+      ggplot() +
+      geom_col(aes(Type, perc, fill = name)) +
+      scale_x_discrete(name = "") +
+      scale_y_continuous(name = "% share",
+                         limits = c(-1, 1), expand = c(0, 0),
+                         labels = function(x) paste0(abs(x * 100), "%"),
+                         breaks = seq(-1, 1, 0.2)) +
+      coord_flip() +
+      theme_bw(15) +
+      theme(legend.title = element_blank(),
+            legend.position = "bottom")
+  })
+  
+  output$table <- DT::renderDT(filtered_df(), rownames = FALSE, options = list(dom = "tip"))
+  
+  output$filtered_table <- downloadHandler(
+    filename = function() { paste0("data-", Sys.Date(), ".csv") },
+    content = function(file) { write_csv(filtered_df(), file) }
+  )
+}
---
+++ ui.R
@@ -0,0 +1,51 @@
+library(shiny)
+library(shinyWidgets)
+library(tidyverse)
+
+options(shiny.autoreload = TRUE)
+
+df <- read_csv(list.files(pattern = ".csv")[[1]])
+levels <- c("All levels", distinct(df, Level) %>% arrange(Level) %>% pull(Level))
+ssas <- c("All sector subject areas", distinct(df, `Sector Subject Area`) %>%
+  arrange(`Sector Subject Area`) %>%
+  pull(`Sector Subject Area`))
+
+pts <- list("All qualifications",
+            `Included in performance tables` = "Yes",
+            `Not included in performance tables` = "No")
+types <- list("All qualification types" = "all",
+             "Excluding GCSE, AS and A levels" = "excl")
+
+ui <- fluidPage(
+  h3("This interactive visualisation shows the current qualification landscape for qualifications regulated by Ofqual"),
+  br(),
+  p("The availability of qualifications is obtained by blah blah blah"),
+  fluidRow(
+    column(4, selectInput("level", "Qualification level", levels, width = "100%")),
+    column(4, selectInput("ssa", "Sector subject area:", ssas, width = "100%")),
+    column(4, selectInput("pt", "Performance tables:", pts, width = "100%"))
+  ),
+  br(),
+  fluidRow(
+    column(4, selectInput("type", "Qualification type:", types, width = "100%")),
+    column(4, selectInput("order", "Order by:" , c("Certificates", "Available qualifications"), width = "100%")),
+    column(4, searchInput("search", "Search for term in qualification title:",
+                          btnSearch = icon("search"), btnReset = icon("remove"),
+                          width = "100%"))
+  ),
+  fluidRow(
+    plotOutput("plot")
+  ),
+  br(),
+  fluidRow(
+    DT::dataTableOutput("table")
+  ),
+  br(),
+  fluidRow(
+    downloadButton("filtered_table", label = "Download this table")
+  ),
+  fluidRow(
+    p("Made by Mark for James")
+  ),
+  tags$head(tags$style(type="text/css", ".container-fluid {  max-width: 950px; }"))
+)
